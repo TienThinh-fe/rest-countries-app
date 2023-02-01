b1fbd6d3475e48f67efd233015b5b02039cf04ab
tienthinh-fe <thinhhtde150233@fpt.edu.vn>
2023-02-01 11:21:21 +0700

    fix: add key into preview component

--- src/components/PreviewList.tsx
+++ src/components/PreviewList.tsx
@@ -5,8 +5,8 @@ import { dumbData, CountryType, CountryListType } from "../data";
 export function PreviewList() {
   return (
     <div className="preview-list">
-      {dumbData.countries.map((country: CountryType) => (
-        <Preview country={country} />
+      {dumbData.countries.map((country: CountryType, index: number) => (
+        <Preview key={index} country={country} />
       ))}
     </div>
   );
